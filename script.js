4306f092d55d105dedd2934f67cd621fb82b3e2c
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-19 22:04:17 -0800

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let draggedElement = null;
       const target = event.target.closest('.image');
 
       if (target && target !== draggedElement) {
-        // Swap the inner HTML of the dragged and target elements
+        
         const draggedHTML = draggedElement.innerHTML;
         draggedElement.innerHTML = target.innerHTML;
         target.innerHTML = draggedHTML;
