560d32f11e553aa0d6a534f6c09b2a503f292eab
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-19 22:18:53 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 let draggedElement = null;
 
     function dragStart(event) {
-      draggedElement = event.target.closest('.image');
+      draggedElement = event.target.closest('.image-container');
       draggedElement.classList.add('dragging');
     }
 
     function allowDrop(event) {
-      event.preventDefault();
+      event.preventDefault(); // Necessary to allow dropping
     }
 
     function drop(event) {
       event.preventDefault();
-      const target = event.target.closest('.image');
-
+      const target = event.target.closest('.image-container');
+      
       if (target && target !== draggedElement) {
-        
+        // Swap inner HTML between dragged and target elements
         const draggedHTML = draggedElement.innerHTML;
         draggedElement.innerHTML = target.innerHTML;
         target.innerHTML = draggedHTML;
 
+        // Remove the dragging class
         draggedElement.classList.remove('dragging');
       }
-    }
+	}	
