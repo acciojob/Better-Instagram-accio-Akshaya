63fa5ff5c53da360b7cd3c4df45eda2163d40176
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-19 20:50:39 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
-// Add drag and drop event listeners
-draggableElements.forEach((element) => {
-  element.addEventListener('dragstart', (event) => {
-    // Store the ID of the dragged element
-    event.dataTransfer.setData('text/plain', event.target.id);
-    // Add a class to visually indicate dragging
-    event.target.classList.add('selected');
-  });
+var dragElements = document.querySelectorAll('.drag');
+for(var i = 0; i < dragElements.length; i++) {
+    dragElements[i].setAttribute('draggable', 'true');
+}
+for(var i = 0; i < dragElements.length; i++) {
+    dragElements[i].addEventListener('dragstart', function(event) {
+        event.dataTransfer.setData('text/plain', event.target.id);
+    });
 
-  element.addEventListener('dragend', (event) => {
-    // Remove the visual indicator class
-    event.target.classList.remove('selected');
-  });
-
-  element.addEventListener('dragover', (event) => {
-    // Allow the drop by preventing the default behavior
-    event.preventDefault();
-    // Add a hover effect
-    event.target.classList.add('hover');
-  });
-
-  element.addEventListener('dragleave', (event) => {
-    // Remove the hover effect
-    event.target.classList.remove('hover');
-  });
-
-  element.addEventListener('drop', (event) => {
-    // Prevent default behavior
-    event.preventDefault();
-    // Remove the hover effect
-    event.target.classList.remove('hover');
-    // Get the ID of the dragged element
-    const draggedId = event.dataTransfer.getData('text/plain');
-    const draggedElement = document.getElementById(draggedId);
-    // Get the target element
-    const targetElement = event.target;
-    // Ensure both elements exist and are not the same
-    if (draggedElement && draggedElement !== targetElement) {
-      // Swap the background images of the dragged and target elements
-      swapBackgroundImages(draggedElement, targetElement);
-    }
-  });
+    dragElements[i].addEventListener('dragend', function(event) {
+        // You can add code here to do something when the drag ends if you want
+    });
+}
+var container = document.querySelector('.container');
+container.addEventListener('dragover', function(event) {
+    event.preventDefault(); // This is necessary to allow a drop
 });
 
-function swapBackgroundImages(el1, el2) {
-  const temp = el1.style.backgroundImage;
-  el1.style.backgroundImage = el2.style.backgroundImage;
-  el2.style.backgroundImage = temp;
-}
+container.addEventListener('drop', function(event) {
+    event.preventDefault(); // This is necessary to allow a drop
+
+    var draggedElementId = event.dataTransfer.getData('text/plain');
+    var draggedElement = document.getElementById(draggedElementId);
+
+    var dropTarget = event.target;
+    var temp = document.createElement('div');
+    
+    container.insertBefore(temp, draggedElement);
+    container.insertBefore(draggedElement, dropTarget);
+    container.insertBefore(dropTarget, temp);
+    container.removeChild(temp);
+});
